f8ed85453cd4c6d78c2fbcc8d6c8f9542297e4fe
nakulGoyal19 <nin25ja02@gmail.com>
2019-07-23 15:43:14 +0530

    update

--- maxContiguousSeriesOf_1's.c
+++ maxContiguousSeriesOf_1's.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 int main(){
-int a[10]={0,1,1,0,1,1,0,0,1,1};
-int n=10; int N=1; int max=0;
-    int i=0,l=0,r=0,c=N;
+    int n,i=0,l=0,r=0,c,N,max=0,si=0,ei=0;
+    printf("Enter size of binary array: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("\nEnter elements in array (0 or 1 only): ");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    printf("\nEnter no. of changable 0's: ");
+    scanf("%d",&N);
+    c=N;
     max=1;
     while(r<n-1)
     {
@@ -37,8 +44,9 @@ int n=10; int N=1; int max=0;
        // printf("%d %d  ",l,r);
         if(max<(r-l+1))
         {
+            si=l;ei=r;
             max=r-l+1;
         }
     }
-    printf("%d",max);
+    printf("\nMax continuous series of 1 is: %d from index %d to %d",max,si,ei);
 }
