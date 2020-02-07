838726316e4bf3b592747ac05bb5661c337cad68
nakulGoyal19 <nakul19goyal@gmail.com>
2020-02-07 14:44:04 +0530

    DP jump array game

---
+++ Dynamic Programming/jump-game-array_interviewBit.cpp
@@ -0,0 +1,13 @@
+int Solution::canJump(vector<int> &A) {
+    int temp[A.size()];
+    int i=0;
+    temp[0]=A[0];
+    for(i=1;i<A.size();i++)
+    {
+        int max=A[i-1]-1>temp[i-1]-1?A[i-1]-1:temp[i-1]-1;
+        if(max<0&&i<A.size()-1)
+            return 0;
+        temp[i]=max;
+    }
+    return 1;
+}
